f5ee38156474a11ff081ec2f1a5c0f4ee5f0a5a9
Sani Rogers <sanirogers138@gmail.com>
2023-06-20 19:48:52 -0400

    work on form

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { CreateThoughtComponent } from './create-thought/create-thought.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "landing", component: LandingComponent },
+  { path: "create", component: CreateThoughtComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { CreateThoughtComponent } from './create-thought/create-thought.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    CreateThoughtComponent,
   ],
   imports: [
     BrowserModule,
